3491d0ea9c7477fe0c7b8b1b3d0f9379c58e9db3
Mukesh <mukeshpradhan54325@gmail.com>
2025-08-23 20:34:16 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
-import CountUp from 'react-countup';
-import { motion } from 'framer-motion';
-import { FaStar } from 'react-icons/fa';
-import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import ProductDetailPage from "./components/Productdetail";
 
-
-import HomePage from './components/HomePage';
-
-
-// Sample reviews data
-// const reviews = [
-//   {
-//     avatar: 'https://randomuser.me/api/portraits/women/44.jpg',
-//     name: 'Jane Doe',
-//     date: 'March 2025',
-//     rating: 5,
-//     text: 'Great service and support. Highly recommended!',
-//   },
-//   {
-//     avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
-//     name: 'John Smith',
-//     date: 'April 2025',
-//     rating: 4,
-//     text: 'Very helpful and professional team.',
-//   },
-//   {
-//     avatar: 'https://randomuser.me/api/portraits/women/68.jpg',
-//     name: 'Alice Johnson',
-//     date: 'May 2025',
-//     rating: 5,
-//     text: 'Found my dream job with their help!',
-//   },
-//   {
-//     avatar: 'https://randomuser.me/api/portraits/men/22.jpg',
-//     name: 'Bob Williams',
-//     date: 'April 2025',
-//     rating: 4,
-//     text: 'Good academic guidance and career advice.',
-//   },
-// ];
-
-function App() {
+export default function App() {
   return (
-    <div className="min-h-screen bg-white font-sans">
-     
-<HomePage />
-    
-     
-    </div>
+     <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/product/:id" element={<ProductDetailPage />} />
+    </Routes>
   );
-}
 
-export default App;
+}
